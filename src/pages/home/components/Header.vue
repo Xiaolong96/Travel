84b5701c83d3d3e1a325662d6dbb1e12047973a3
xlupc <xiaolongupc@163.com>
2018-06-02 15:30:38 +0800

    add HomeHeader

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header_left">
+      <div class="iconfont back_icon">&#xe624;</div>
+    </div>
+    <div class="header_input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header_right">
+      城市
+      <span class="iconfont arrow_icon">&#xe6aa;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variable.styl'
+  .header
+    display: flex
+    height: .86rem
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header_left
+      width: .64rem
+      float: left
+      .back_icon
+        text-align: center
+        font-size: .4rem
+    .header_input
+      flex: 1
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      height: .62rem
+      line-height: .62rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header_right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow_icon
+        font-size: .24rem
+</style>
