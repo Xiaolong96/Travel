7b9a3b0cf1cf1c8ca1966487b63f8fbe20c96fd5
xlupc <xiaolongupc@163.com>
2018-06-03 15:50:23 +0800

    add recommend and weekend function

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="recommend">
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="recItem of recommendList" :key="recItem.id">
+        <img class="item-img" :src="recItem.src" alt="">
+        <div class="item-info">
+          <p class="item-title">{{recItem.title}}</p>
+          <p class="item-desc">{{recItem.desc}}</p>
+          <button class="item-btn">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '01',
+        src: 'http://img1.qunarzz.com/sight/p0/1604/78/78873494f26e554090.water.jpg_200x200_331422ad.jpg',
+        title: '京东石林峡',
+        desc: '谷内山峰挺立峭拔，天上美景落人间'
+      }, {
+        id: '02',
+        src: 'http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_200x200_740720ff.jpg',
+        title: '古北水镇',
+        desc: '弄堂小道曲径通幽，古建大宅庭院深深'
+      }, {
+        id: '03',
+        src: 'http://img1.qunarzz.com/sight/p0/1603/ad/ad84d5339c3a34f390.water.jpg_200x200_788cb0d2.jpg',
+        title: '十渡风景区',
+        desc: '秋天的孤山寨蓝天白云映衬，还可欣赏红叶美景'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+  .title
+    height: .8rem
+    line-height: .8rem
+    text-indent: .2rem
+    background: #eee
+  .item
+    overflow: hidden
+    display: flex
+    width: 100%
+    height: 2.2rem
+    .item-img
+      display: block
+      width: 1.8rem
+      height: 1.8rem
+      padding: .2rem
+    .item-info
+      flex: 1
+      padding: .2rem
+      min-width: 0  //？
+      .item-title
+        line-height: .6rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .5rem
+        color: #ccc
+        ellipsis()
+      .item-btn
+        line-height: .4rem
+        margin-top: .2rem
+        background: #ff9300
+        padding: 0 .1rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="recommend">
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.src" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '01',
+        src: 'http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg',
+        title: '寻找北京的皇城范儿',
+        desc: '数百年的宫廷庙宇，至今依旧威严霸气'
+      }, {
+        id: '02',
+        src: 'http://img1.qunarzz.com/sight/source/1505/ce/bc89bc2f0e33ea.jpg_r_640x214_3e408453.jpg',
+        title: '学生最爱的博物馆',
+        desc: '周末干嘛？北京很多博物馆已经免费开放啦'
+      }, {
+        id: '03',
+        src: 'http://img1.qunarzz.com/sight/source/1507/18/2d554e22faa25f.jpg_r_640x214_2a966a54.jpg',
+        title: '爱上庄园',
+        desc: '庄园让臭美的你秒变白富美，高富帅'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+  .title
+    height: .8rem
+    line-height: .8rem
+    text-indent: .2rem
+    background: #eee
+  .item-img-wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .2rem
+    .item-title
+      line-height: .6rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .5rem
+      color: #999
+      ellipsis()
+</style>
